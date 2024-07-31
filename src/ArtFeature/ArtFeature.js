85169d9649a0905f68c308e6316621444c857f13
Zach Wolek <zach.wolek@gmail.com>
2024-07-31 15:44:57 -0500

    feat: add functioning back button to homepage

--- src/ArtFeature/ArtFeature.js
+++ src/ArtFeature/ArtFeature.js
@@ -1,3 +1,6 @@
+import './ArtFeature.css'
+import { Link } from 'react-router-dom'
+
 export const ArtFeature = ({artFeature}) => {
     const {artist_title, date_display, description, id, image_id, place_of_origin, short_description, style_title, title} = artFeature
 
@@ -8,13 +11,16 @@ export const ArtFeature = ({artFeature}) => {
             <p>date_display: {date_display}</p>
             <div dangerouslySetInnerHTML={{ __html: description }} />
             <p>id: {id}</p>
-            <img src={`https://www.artic.edu/iiif/2/${image_id}/full/250,/0/default.jpg`}/>
+            <img src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`}/>
             <p>place_of_origin: {place_of_origin}</p>
             <p>short_description: {short_description}</p>
             <p>style_title: {style_title}</p>
             <p>title: {title}</p>
             <button>SAVE</button>
+            <Link to={`/`}>
             <button>BACK</button>
+            </Link>  
         </>
     )
 }
+
