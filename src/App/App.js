b6f55bedd30ff9090357c79a84827c004d6ff4c2
Zach Wolek <zach.wolek@gmail.com>
2024-07-31 11:46:45 -0500

    feat: add component for single piece view

--- src/App/App.js
+++ src/App/App.js
@@ -24,6 +24,9 @@ const App = () => {
                 <Exhibitions exhibitions={exhibitions}/>
               </>
           }/>
+          <Route path='/artfeature/:id' element={
+            <ArtFeature />
+          }/>
       </Routes> 
     </div>
   );
