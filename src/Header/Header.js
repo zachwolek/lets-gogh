5dd2b7d2934bb54c873506f5cdacfc5f3d853fb6
Zach Wolek <zach.wolek@gmail.com>
2024-07-31 21:30:26 -0500

    style: edit header to contain subtitle

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 export const Header = ({setSearchValue}) => {
     return (
     <header>
-        <h1>Let's Gogh</h1>
+        <h1>Let's Gogh!</h1>
+        <h2>To the Art Institute of Chicago</h2>
         <Form setSearchValue={setSearchValue}/>
         <Link to="/saved" className="saved-link">Saved Exhibitions</Link>
      </header>
