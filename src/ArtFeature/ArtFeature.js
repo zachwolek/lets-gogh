96273b797cc677faf1f4eb05b83d1a0ac498f083
Zach Wolek <zach.wolek@gmail.com>
2024-08-03 17:25:50 -0500

    style: update Art Feature Card

--- src/ArtFeature/ArtFeature.js
+++ src/ArtFeature/ArtFeature.js
@@ -8,15 +8,13 @@ export const ArtFeature = ({artFeature, savedExhibitionIds, toggleSaveExhibition
     console.log("DESCRIPTION: ", artFeature)
     return (
         <>
-            <p className='artist_title'>artist_title: {artist_title}</p>
-            <p className='date_display'>date_display: {date_display}</p>
-            <p className="description" dangerouslySetInnerHTML={{ __html: description }} />
-            <p>id: {id}</p>
+            <h1 className='title'>{title}</h1>
+            {artist_title && <h2 className='artist_title'>{artist_title}</h2>}
             <img src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`}/>
+            {description && <p className="description" dangerouslySetInnerHTML={{ __html: description }} />}
             <p className='place_of_origin'>place_of_origin: {place_of_origin}</p>
-            <p dangerouslySetInnerHTML={{ __html: short_description }} />
+            <p className='date_display'>date_display: {date_display}</p>
             <p className='style_title'>style_title: {style_title}</p>
-            <p className='title'>title: {title}</p>
             <Link to={`/`}>
                 <button className='home_button'>HOME</button>
             </Link>  
